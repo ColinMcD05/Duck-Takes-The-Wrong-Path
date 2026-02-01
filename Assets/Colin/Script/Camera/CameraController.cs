0b27052a70b4146e62805097cfa1451c86642790
ColinMcD05 <colinmcd05@gmail.com>
2026-01-31 23:28:36 -0500

    Camera
    Added Camera logic.
    Player cannot leave left side of screen and camera moves with the right

--- Assets/Colin/Script/Camera/CameraController.cs
+++ Assets/Colin/Script/Camera/CameraController.cs
@@ -5,18 +5,29 @@ using System.Collections.Generic;
 public class CameraController : MonoBehaviour
 {
 
-    public float leftClamp;
+    [SerializeField] GameObject player;
+    public float center;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        leftClamp = -Camera.main.orthographicSize;
-
+        center = transform.position.x - 0.5f;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        if (!player.GetComponent<PlayerMovement>().playerDirection)
+        {
+            if (player.transform.position.x >= center)
+            {
+                transform.position += new Vector3(player.GetComponent<PlayerMovement>().hMovement, 0f) * Time.deltaTime * (player.GetComponent<PlayerMovement>().playerSpeed);
+                player.GetComponent<PlayerMovement>().leftClamp += player.GetComponent<PlayerMovement>().hMovement * Time.deltaTime * player.GetComponent<PlayerMovement>().playerSpeed;
+                center += player.GetComponent<PlayerMovement>().hMovement * Time.deltaTime * player.GetComponent<PlayerMovement>().playerSpeed;
+            }
+            else if (player.transform.position.x > (center - 2.5) && player.transform.position.x < center)
+            {
+                transform.position += new Vector3(player.GetComponent<PlayerMovement>().hMovement, 0f) * Time.deltaTime * (player.GetComponent<PlayerMovement>().playerSpeed / 2);
+                player.GetComponent<PlayerMovement>().leftClamp += player.GetComponent<PlayerMovement>().hMovement * Time.deltaTime * player.GetComponent<PlayerMovement>().playerSpeed / 2;
+                center += player.GetComponent<PlayerMovement>().hMovement * Time.deltaTime * player.GetComponent<PlayerMovement>().playerSpeed/2;
+            }
+        }
     }
 }
--- Assets/Colin/Script/Player/PlayerMovement.cs
+++ Assets/Colin/Script/Player/PlayerMovement.cs
@@ -8,34 +8,40 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] Rigidbody2D playerRigidbody;
     public float playerSpeed;
-    public int playerDirection = 1;
+    public bool playerDirection = false;
     public float hMovement;
     [SerializeField] SpriteRenderer spriteRenderer;
     public float leftClamp;
-    
+
+    void Start()
+    {
+        leftClamp = (-Camera.main.orthographicSize * Camera.main.aspect) + 0.5f;
+    }
 
     // Update is called once per frame
     void Update()
     {
         GetInput();
-        leftClamp = -Camera.main.orthographicSize;
-        Debug.Log(leftClamp);
-    }
-
-    private void FixedUpdate()
-    {
         Movement();
     }
 
     void GetInput()
     {
         hMovement = Input.GetAxis("Horizontal");
+        if (hMovement > 0)
+        {
+            playerDirection = false;
+        }
+        else if (hMovement < 0)
+        {
+            playerDirection = true;
+        }
     }
 
     void Movement()
     {
-        playerRigidbody.linearVelocityX = hMovement * playerSpeed;
-        spriteRenderer.flipX = playerRigidbody.linearVelocity.x < 0f;
+        transform.Translate(new Vector2(hMovement, 0f) *Time.deltaTime * playerSpeed);
+        spriteRenderer.flipX = playerDirection;
         if (transform.position.x <= leftClamp)
         {
             transform.position = new Vector2(leftClamp, transform.position.y);
