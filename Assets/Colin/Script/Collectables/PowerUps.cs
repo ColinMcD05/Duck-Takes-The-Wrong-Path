e7d12898c05ff206b7ab5ead43fbd96f00e22268
ColinMcD05 <colinmcd05@gmail.com>
2026-02-02 22:13:58 -0500

    Minor Changes

--- Assets/Colin/Script/Collectables/PowerUps.cs
+++ Assets/Colin/Script/Collectables/PowerUps.cs
@@ -3,15 +3,11 @@ using UnityEngine;
 
 public class PowerUps : MonoBehaviour
 {
-    public int pointValue;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (collision.GetComponent<PlayerController>().currentPower != gameObject.name)
-            {
-                collision.GetComponent<PlayerPowers>().SwitchPower(gameObject.name);
-            }
+            collision.GetComponent<PlayerController>().SwitchPower(gameObject.name);
         }
     }
 }
--- Assets/Colin/Script/Player/PlayerPowers.cs
+++ Assets/Colin/Script/Player/PlayerPowers.cs
@@ -2,29 +2,8 @@ using UnityEngine;
 
 public class PlayerPowers : MonoBehaviour
 {
-    public void SwitchPower(string gainedPower)
+    public void Grow()
     {
-        switch (gainedPower)
-        {
-            case "Grow":
-                if (gameObject.GetComponent<PlayerController>().currentPower == "Defualt")
-                {
-                    gameObject.GetComponent<PlayerController>().currentPower = "Grow";
-                    Debug.Log(gameObject.GetComponent<PlayerController>().currentPower);
-                }
-                break;
-            case "Water":
-                gameObject.GetComponent<PlayerController>().currentPower = "Water";
-                Debug.Log(gameObject.GetComponent<PlayerController>().currentPower);
-                break;
-            case "Invincible":
-                gameObject.GetComponent<PlayerController>().invincible = true;
-                Debug.Log(gameObject.GetComponent<PlayerController>().currentPower);
-                break;
-            case null:
-                gameObject.GetComponent<PlayerController>().currentPower = "Defualt";
-                Debug.Log(gameObject.GetComponent<PlayerController>().currentPower);
-                break;
-        }
+
     }
 }
